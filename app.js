76da50c68eb3e5e392a6ee843258e1fc913bce55
vuongkhanh1403 <131775979+vuongkhanh1403@users.noreply.github.com>
2023-05-27 16:33:14 +0700

    Web_ZooFast_file

---
+++ app.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function(){
+    $('#eye').click(function(){
+        $(this).toggleClass('open')
+        $(this).children('i').toggleClass('fa-eye-slash fa-eye');
+        if($(this).hasClass('open')){
+            alert('Type text');
+            $(this).prev().attr('type', 'text');
+        }else{
+            $(this).prev().attr('type', 'password');
+        }
+    });
+    
+});
